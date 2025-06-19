37801ed23756615c14dab29d3260250f3e84d9e9
OligoCodes <josephoseibonsu742@gmail.com>
2025-06-19 11:26:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const generatePassword = () => {
-  let inputValue = parseInt(document.getElementById("char").value);
-  fetch(`https://password-generator-v2.p.rapidapi.com/api/password-generator?length=${inputValue}`, {
-      method: "GET",
-      headers: { 'x-rapidapi-host': 'password-generator-v2.p.rapidapi.com', 'x-rapidapi-key': 'da949b45e5mshd847ffdf187b908p16f82fjsn301400523fe1' }
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      document.getElementById("pass").textContent = `${data["generated-password"]}`;
-    })
-    .catch(error => {
-      console.error(error)
-    })
-};
+      const inputValue = parseFloat(document.getElementById("char").value);
+      const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+      let password = "";
+      for (let i = 0; i < inputValue; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+      }
+      document.getElementById("pass").textContent = password;
+  };
